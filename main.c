cf837710c85ab97cb39749393f54938b3b140557
joaorobertoaquino <galvaojoaoroberto@gmail.com>
2024-10-14 15:33:46 -0300

    Navegação do Módulo Relatórios.
    Navega para as funções de "listarClientes", "listarBicicletas" e "listarPatinetes".

--- main.c
+++ main.c
@@ -86,31 +86,6 @@ int main(){
     }while(op_pric != 0);
     return 0;
 }
-//     cadastrarCliente();
-//     pesquisarCliente();
-//     atualizarCliente();
-//     deletarCliente();
-
-//     cadastrarBicicleta();
-//     pesquisarBicicleta();
-//     atualizarBicicleta();
-//     deletarBicicleta();
-
-//     cadastrarPatinete();
-//     pesquisarPatinete();
-//     atualizarPatinete();
-//     deletarPatinete();
-
-//     reservarBicicleta();
-//     reservarPatinete();
-//     devolverBicicleta();
-//     devolverPatinete();
-
-//     listarClientes();
-//     listarBicicletas();
-//     listarPatinetes();
-//     consultarAluguel();
-// }
 
 
 void telaInicial(void){
@@ -560,51 +535,72 @@ void consultarAluguel(void){
 }
 
 void relatorios(void){
-    char op_relatorio;
-    printf("\n*-------------------------------------------------------------------------------*\n");
-    printf("*                                Módulo Relatório                               *\n");
-    printf("*-------------------------------------------------------------------------------*\n");
-    printf("|                             1. Lista Geral de Clientes                        |\n");
-    printf("|                             2. Lista Geral de Bicicletas                      |\n");
-    printf("|                             3. Lista Geral de Patinetes                       |\n");
-    printf("|                             4. Veículos mais Alugados                         |\n");
-    printf("|                             5. Histórico de Alugueis                          |\n");
-    printf("|                             0. Voltar                                         |\n");
-    printf("*-------------------------------------------------------------------------------*\n");
-    printf("| Digite a opção desejada:                                                      |\n");
-    scanf("%c", &op_relatorio);
-    getchar();
-    printf("*-------------------------------------------------------------------------------*\n");
-    printf("Tecle <ENTER> para continuar...");
-    getchar();
-    
+    int op_relatorio;
+    do{
+        system("clear||cls");
+        printf("\n*-------------------------------------------------------------------------------*\n");
+        printf("*                                Módulo Relatório                               *\n");
+        printf("*-------------------------------------------------------------------------------*\n");
+        printf("|                             1. Lista Geral de Clientes                        |\n");
+        printf("|                             2. Lista Geral de Bicicletas                      |\n");
+        printf("|                             3. Lista Geral de Patinetes                       |\n");
+        // printf("|                             4. Veículos mais Alugados                         |\n");
+        // printf("|                             5. Histórico de Alugueis                          |\n");
+        printf("|                             0. Voltar                                         |\n");
+        printf("*-------------------------------------------------------------------------------*\n");
+        printf("| Digite a opção desejada:                                                      |\n");
+        scanf("%d", &op_relatorio);
+        getchar();
+        switch (op_relatorio){
+            case 1:
+                listarClientes();
+                break;
+            case 2:
+                listarBicicletas();
+                break;
+            case 3:
+                listarPatinetes();
+                break;
+            case 0:
+                break;
+            default:
+                printf("Valor inválido");
+                break;
+        }
+    }while (op_relatorio != 0);
 }
 
 void listarClientes(void){
+    system("clear||cls");
     printf("\n*------------------------------------------------------------------------------------------------------------*\n");
     printf("*                                          Listar Clientes                                                   *\n");
     printf("*------------------------------------------------------------------------------------------------------------*\n");
     printf("|       Nome        |       CPF       |           Email             |      Fone      |   Data de Nacimento   |\n");
     printf("*------------------------------------------------------------------------------------------------------------*\n");
     printf("Tecle <ENTER> para continuar...");
+    getchar();
 }
 
 void listarBicicletas(void){
+    system("clear||cls");
     printf("\n*------------------------------------------------------------------------------------------------------------*\n");
     printf("*                                         Listar Bicicletas                                                  *\n");
     printf("*------------------------------------------------------------------------------------------------------------*\n");
     printf("|     ID     |         Modelo          |         Marca          | Ano de Fabricação |   Tamanho do Quadro    |\n");
     printf("*------------------------------------------------------------------------------------------------------------*\n");
     printf("Tecle <ENTER> para continuar...");
+    getchar();
 }
 
 void listarPatinetes(void){
+    system("clear||cls");
     printf("\n*------------------------------------------------------------------------------------------------------------*\n");
     printf("*                                         Listar Patinetes                                                   *\n");
     printf("*------------------------------------------------------------------------------------------------------------*\n");
     printf("|     ID     |         Modelo          |         Marca          | Ano de Fabricação |  Bateria (capacidade)  |\n");
     printf("*------------------------------------------------------------------------------------------------------------*\n");
     printf("Tecle <ENTER> para continuar...");
+    getchar();
 }
 
 void informacoes(void){
