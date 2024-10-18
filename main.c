447cc51da709b78199264f32c50c49c44d6335c4
ariadny <ariadnyfran15@gmail.com>
2024-10-18 15:56:40 -0300

    Declarando a função "bicicleta_mais_alugadas" no arquivo "relatorios.h"

--- main.c
+++ main.c
@@ -24,7 +24,6 @@ void devolverPatinete(void);
 void consultarAluguel(void);
 
 //Relatórios
-void bicicleta_mais_alugadas(void);
 void patinetes_mais_alugados(void);
 
 //Informações
--- relatorios/relatorios.h
+++ relatorios/relatorios.h
@@ -5,5 +5,6 @@ void relatorios(void);
 void listarClientes(void);
 void listarBicicletas(void);
 void listarPatinetes(void);
+void bicicleta_mais_alugadas(void);
 
 #endif
