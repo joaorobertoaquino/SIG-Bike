8a8f9cf58e6b252176fde8a53425df6bd8820615
joaorobertoaquino <galvaojoaoroberto@gmail.com>
2024-10-19 10:11:18 -0300

    Versão 5.0 - Edição da interface do Módulo Clientes.

--- clientes/clientes.c
+++ clientes/clientes.c
@@ -7,16 +7,16 @@ void clientes(void){
     do{
         system("clear||cls");
         printf("\n");
-        printf("\n*---------------------------------------------------------------------------------*\n");
-        printf("*                               Módulo Clientes                                   *\n");
-        printf("*---------------------------------------------------------------------------------*\n");
-        printf("|                               1. Cadastrar                                      |\n");
-        printf("|                               2. Pesquisar                                      |\n");
-        printf("|                               3. Atualizar                                      |\n");
-        printf("|                               4. Deletar                                        |\n");
-        printf("|                               0. Voltar                                         |\n");
-        printf("|                                                                                 |\n");
-        printf("*---------------------------------------------------------------------------------*\n");
+        printf("\n╔═════════════════════════════════════════════════════════════════════════════════╗\n");
+        printf("║                               Módulo Clientes                                   ║\n");
+        printf("╚═════════════════════════════════════════════════════════════════════════════════╝\n");
+        printf("║                               1. Cadastrar                                      ║\n");
+        printf("║                               2. Pesquisar                                      ║\n");
+        printf("║                               3. Atualizar                                      ║\n");
+        printf("║                               4. Deletar                                        ║\n");
+        printf("║                               0. Voltar                                         ║\n");
+        printf("║                                                                                 ║\n");
+        printf("╚═════════════════════════════════════════════════════════════════════════════════╝\n");
         printf(" Digite a opção desejada: ");
         scanf("%d", &op_cliente);
         getchar();
@@ -44,67 +44,67 @@ void clientes(void){
 
 void cadastrarCliente(void){
     system("clear||cls");
-    printf("\n*-------------------------------------------------------------------------------*\n");
-    printf("*                              Cadastrar Cliente                                *\n");
-    printf("*-------------------------------------------------------------------------------*\n");
-    printf("|                                                                               |\n");
-    printf("| Nome:                                                                         |\n");
-    printf("| CPF:                                                                          |\n");
-    printf("| Email:                                                                        |\n");
-    printf("| Fone:                                                                         |\n");
-    printf("| Data de Nascimento:                                                           |\n");
-    printf("|                                                                               |\n");
-    printf("|                        Cliente cadastrado com sucesso!                        |\n");
-    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                              Cadastrar Cliente                                ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("║                                                                               ║\n");
+    printf("║ Nome:                                                                         ║\n");
+    printf("║ CPF:                                                                          ║\n");
+    printf("║ Email:                                                                        ║\n");
+    printf("║ Fone:                                                                         ║\n");
+    printf("║ Data de Nascimento:                                                           ║\n");
+    printf("║                                                                               ║\n");
+    printf("║                        Cliente cadastrado com sucesso!                        ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("Tecle <ENTER> para continuar...");
     getchar();
 }
 
 void pesquisarCliente(void){
     system("clear||cls");
-    printf("\n*-------------------------------------------------------------------------------*\n");
-    printf("*                          Pesquisar Dados do Cliente                           *\n");
-    printf("*-------------------------------------------------------------------------------*\n");
-    printf("| Digite o CPF do Cliente:                                                      |\n");
-    printf("|                                                                               |\n");
-    printf("| Nome:                                                                         |\n");
-    printf("| CPF:                                                                          |\n");
-    printf("| Email:                                                                        |\n");
-    printf("| Fone:                                                                         |\n");
-    printf("| Data de Nascimento:                                                           |\n");
-    printf("|                                                                               |\n");
-    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                          Pesquisar Dados do Cliente                           ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("║ Digite o CPF do Cliente:                                                      ║\n");
+    printf("║                                                                               ║\n");
+    printf("║ Nome:                                                                         ║\n");
+    printf("║ CPF:                                                                          ║\n");
+    printf("║ Email:                                                                        ║\n");
+    printf("║ Fone:                                                                         ║\n");
+    printf("║ Data de Nascimento:                                                           ║\n");
+    printf("║                                                                               ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("Tecle <ENTER> para continuar...");
     getchar();
 }
 
 void atualizarCliente(void){
     system("clear||cls");
-    printf("\n*-------------------------------------------------------------------------------*\n");
-    printf("*                         Atualizar Dados do Cliente                            *\n");
-    printf("*-------------------------------------------------------------------------------*\n");
-    printf("| Digite o CPF do Cliente:                                                      |\n");
-    printf("|                                                                               |\n");
-    printf("|                           ↪Digite os Novos Dados↩                             |\n");
-    printf("| Nome:                                                                         |\n");
-    printf("| CPF:                                                                          |\n");
-    printf("| Email:                                                                        |\n");
-    printf("| Fone:                                                                         |\n");
-    printf("| Data de Nascimento:                                                           |\n");
-    printf("|                                                                               |\n");
-    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                         Atualizar Dados do Cliente                            ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("║ Digite o CPF do Cliente:                                                      ║\n");
+    printf("║                                                                               ║\n");
+    printf("║                           ↪Digite os Novos Dados↩                             ║\n");
+    printf("║ Nome:                                                                         ║\n");
+    printf("║ CPF:                                                                          ║\n");
+    printf("║ Email:                                                                        ║\n");
+    printf("║ Fone:                                                                         ║\n");
+    printf("║ Data de Nascimento:                                                           ║\n");
+    printf("║                                                                               ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("Tecle <ENTER> para continuar...");
     getchar();
 }
 
 void deletarCliente(void){
     system("clear||cls");
-    printf("\n*-------------------------------------------------------------------------------*\n");
-    printf("*                               Excluir Cliente                                 *\n");
-    printf("*-------------------------------------------------------------------------------*\n");
-    printf("| Digite o CPF do Cliente:                                                      |\n");
-    printf("|                                                                               |\n");
-    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                               Excluir Cliente                                 ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("║ Digite o CPF do Cliente:                                                      ║\n");
+    printf("║                                                                               ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("Tecle <ENTER> para continuar...");
     getchar();
 }
