d8df840bdf5dc86cc74dd03ab080c319948e6aee
joaorobertoaquino <galvaojoaoroberto@gmail.com>
2024-10-19 10:03:52 -0300

    Versão 5.0 - Mudanças nas interfaces do Módulo Bicicletas.

--- bicicletas/bicicletas.c
+++ bicicletas/bicicletas.c
@@ -6,16 +6,16 @@ void bicicletas(void){
     int op_bicicleta;
     do{
         system("clear||cls");
-        printf("\n*-------------------------------------------------------------------------------*\n");
-        printf("*                               Módulo Bicicletas                               *\n");
-        printf("*-------------------------------------------------------------------------------*\n");
-        printf("|                             1. Cadastrar                                      |\n");
-        printf("|                             2. Pesquisar                                      |\n");
-        printf("|                             3. Atualizar                                      |\n");
-        printf("|                             4. Deletar                                        |\n");
-        printf("|                             0. Voltar                                         |\n");
-        printf("|                                                                               |\n");
-        printf("*-------------------------------------------------------------------------------*\n");
+        printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+        printf("║                               Módulo Bicicletas                               ║\n");
+        printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+        printf("║                             1. Cadastrar                                      ║\n");
+        printf("║                             2. Pesquisar                                      ║\n");
+        printf("║                             3. Atualizar                                      ║\n");
+        printf("║                             4. Deletar                                        ║\n");
+        printf("║                             0. Voltar                                         ║\n");
+        printf("║                                                                               ║\n");
+        printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
         printf(" Digite a opção desejada: ");
         scanf("%d", &op_bicicleta);
         getchar();
@@ -43,68 +43,68 @@ void bicicletas(void){
 
 void cadastrarBicicleta(void){
     system("clear||cls");
-    printf("\n*-------------------------------------------------------------------------------*\n");
-    printf("*                               Cadastrar Bicicleta                             *\n");
-    printf("*-------------------------------------------------------------------------------*\n");
-    printf("| ID:                                                                           |\n");
-    printf("|                                                                               |\n");
-    printf("| Modelo:                                                                       |\n");
-    printf("| Cor:                                                                          |\n");
-    printf("| Marca:                                                                        |\n");
-    printf("| Ano de Fabricação:                                                            |\n");
-    printf("| Tamanho do Quadro:                                                            |\n");
-    printf("|                                                                               |\n");
-    printf("|                      Bicicleta cadastrada com sucesso!                        |\n");
-    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                               Cadastrar Bicicleta                             ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("║ ID:                                                                           ║\n");
+    printf("║                                                                               ║\n");
+    printf("║ Modelo:                                                                       ║\n");
+    printf("║ Cor:                                                                          ║\n");
+    printf("║ Marca:                                                                        ║\n");
+    printf("║ Ano de Fabricação:                                                            ║\n");
+    printf("║ Tamanho do Quadro:                                                            ║\n");
+    printf("║                                                                               ║\n");
+    printf("║                      Bicicleta cadastrada com sucesso!                        ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("Tecle <ENTER> para continuar...");
     getchar();
 }
 
 void pesquisarBicicleta(void){
     system("clear||cls");
-    printf("\n*-------------------------------------------------------------------------------*\n");
-    printf("*                               Pesquisar Bicicleta                             *\n");
-    printf("*-------------------------------------------------------------------------------*\n");
-    printf("| Informe o ID da Bicicleta:                                                    |\n");
-    printf("|                                                                               |\n");
-    printf("| Modelo:                                                                       |\n");
-    printf("| Cor:                                                                          |\n");
-    printf("| Marca:                                                                        |\n");
-    printf("| Ano de Fabricação:                                                            |\n");
-    printf("| Tamanho do Quadro:                                                            |\n");
-    printf("|                                                                               |\n");
-    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                               Pesquisar Bicicleta                             ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("║ Informe o ID da Bicicleta:                                                    ║\n");
+    printf("║                                                                               ║\n");
+    printf("║ Modelo:                                                                       ║\n");
+    printf("║ Cor:                                                                          ║\n");
+    printf("║ Marca:                                                                        ║\n");
+    printf("║ Ano de Fabricação:                                                            ║\n");
+    printf("║ Tamanho do Quadro:                                                            ║\n");
+    printf("║                                                                               ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("Tecle <ENTER> para continuar...");
     getchar();
 }
 
 void atualizarBicicleta(void){
     system("clear||cls");
-    printf("\n*-------------------------------------------------------------------------------*\n");
-    printf("*                          Atualizar Dados da Bicicleta                         *\n");
-    printf("*-------------------------------------------------------------------------------*\n");
-    printf("| Informe o ID da Bicicleta:                                                    |\n");
-    printf("|                                                                               |\n");
-    printf("|                           ↪Digite os Novos Dados↩                             |\n");
-    printf("| Modelo:                                                                       |\n");
-    printf("| Cor:                                                                          |\n");
-    printf("| Marca:                                                                        |\n");
-    printf("| Ano de Fabricação:                                                            |\n");
-    printf("| Tamanho do Quadro:                                                            |\n");
-    printf("|                                                                               |\n");
-    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                          Atualizar Dados da Bicicleta                         ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("║ Informe o ID da Bicicleta:                                                    ║\n");
+    printf("║                                                                               ║\n");
+    printf("║                           ↪Digite os Novos Dados↩                             ║\n");
+    printf("║ Modelo:                                                                       ║\n");
+    printf("║ Cor:                                                                          ║\n");
+    printf("║ Marca:                                                                        ║\n");
+    printf("║ Ano de Fabricação:                                                            ║\n");
+    printf("║ Tamanho do Quadro:                                                            ║\n");
+    printf("║                                                                               ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("Tecle <ENTER> para continuar...");
     getchar();
 }
 
 void deletarBicicleta(void){
     system("clear||cls");
-    printf("\n*-------------------------------------------------------------------------------*\n");
-    printf("*                               Excluir Bicicleta                               *\n");
-    printf("*-------------------------------------------------------------------------------*\n");
-    printf("| Informe o ID da bicicleta à excluir:                                          |\n");
-    printf("|                                                                               |\n");
-    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                               Excluir Bicicleta                               ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("║ Informe o ID da bicicleta à excluir:                                          ║\n");
+    printf("║                                                                               ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("Tecle <ENTER> para continuar...");
     getchar();
 }
